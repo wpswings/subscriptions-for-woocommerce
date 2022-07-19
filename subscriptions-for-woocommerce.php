3f1624bc939e0eb9b073c96d29485ebf88b84013
shivamddeveloper <shivamsrivastava@makewebbetter.com>
2022-07-19 12:58:42 +0530

    wc & wp update

--- subscriptions-for-woocommerce.php
+++ subscriptions-for-woocommerce.php
@@ -22,9 +22,9 @@
  * Domain Path:       /languages
  *
  * Requires at least:        5.0
- * Tested up to:             6.0.0
+ * Tested up to:             6.0.1
  * WC requires at least:     5.0
- * WC tested up to:          6.5.0
+ * WC tested up to:          6.7.0
  *
  * License:           GNU General Public License v3.0
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.html
