df8c96b1f953fccec900aefd1efa7628e421af70
shivamddeveloper <shivamsrivastava@makewebbetter.com>
2023-04-13 17:04:38 +0530

    language update

--- subscriptions-for-woocommerce.php
+++ subscriptions-for-woocommerce.php
@@ -22,7 +22,7 @@
  * Domain Path:       /languages
  *
  * Requires at least:        5.1.0
- * Tested up to:             6.1.2
+ * Tested up to:             6.2
  * WC requires at least:     5.1.0
  * WC tested up to:          7.5.1
  *
