8ca069c4f2918d02fdae31d9fc66952709d8ef68
shivamddeveloper <shivamsrivastava@makewebbetter.com>
2023-07-27 16:23:33 +0530

    wc update

--- subscriptions-for-woocommerce.php
+++ subscriptions-for-woocommerce.php
@@ -24,7 +24,7 @@
  * Requires at least:        5.1.0
  * Tested up to:             6.3.0
  * WC requires at least:     5.1.0
- * WC tested up to:          7.9.0
+ * WC tested up to:          8.0.0
  *
  * License:           GNU General Public License v3.0
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.html
