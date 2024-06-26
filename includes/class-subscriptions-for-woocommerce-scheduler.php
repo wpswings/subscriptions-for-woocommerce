5e1340114946238b02f8604548d1c2c12df761b8
mohammaddanish-cedcoss <mohddanish@makewebbetter.com>
2024-06-26 15:10:49 +0530

    fixed-issue

--- includes/class-subscriptions-for-woocommerce-scheduler.php
+++ includes/class-subscriptions-for-woocommerce-scheduler.php
@@ -857,9 +857,9 @@ if ( ! class_exists( 'Subscriptions_For_Woocommerce_Scheduler' ) ) {
 							$parent_order_id = $subscription_id;
 						}
 						// update next payment date.
-						$wps_next_payment_date = wps_sfw_next_payment_date( $subscription_id, $current_time, 0 );
+						// $wps_next_payment_date = wps_sfw_next_payment_date( $subscription_id, $current_time, 0 );
 
-						wps_sfw_update_meta_data( $subscription_id, 'wps_next_payment_date', $wps_next_payment_date );
+						// wps_sfw_update_meta_data( $subscription_id, 'wps_next_payment_date', $wps_next_payment_date );
 
 						// custom filter.
 						if ( apply_filters( 'wps_sfw_stop_recurring_payment_incase_manual', false, $parent_order_id ) ) {
