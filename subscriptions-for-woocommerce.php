b80d6bd502a00c4fce96a4f8c81b56c048cce748
mohammaddanish-cedcoss <mohddanish@makewebbetter.com>
2024-06-27 12:52:43 +0530

    version-update

--- subscriptions-for-woocommerce.php
+++ subscriptions-for-woocommerce.php
@@ -23,9 +23,9 @@
  * Requires Plugins:  woocommerce
  *
  * Requires at least:        5.1.0
- * Tested up to:             6.5.4
+ * Tested up to:             6.5.5
  * WC requires at least:     5.1.0
- * WC tested up to:          9.0.0
+ * WC tested up to:          9.0.2
  *
  * License:           GNU General Public License v3.0
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.html
