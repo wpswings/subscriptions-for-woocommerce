82edd529ec16be9be4bb09eb87453647e95f0604
Shivamddeveloper <shivamsrivastava@makewebbetter.com>
2026-01-13 12:24:22 +0530

    v1.8.10

--- includes/class-subscriptions-for-woocommerce.php
+++ includes/class-subscriptions-for-woocommerce.php
@@ -80,7 +80,7 @@ class Subscriptions_For_Woocommerce {
 			$this->version = SUBSCRIPTIONS_FOR_WOOCOMMERCE_VERSION;
 		} else {
 
-			$this->version = '1.9.0';
+			$this->version = '1.8.10';
 		}
 
 		$this->plugin_name = 'subscriptions-for-woocommerce';
