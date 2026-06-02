4510a33f59cc8d88066edc8e3134ef39998ced6b
Shivamddeveloper <shivamsrivastava@makewebbetter.com>
2026-06-02 14:50:36 +0530

    langauge update

--- subscriptions-for-woocommerce.php
+++ subscriptions-for-woocommerce.php
@@ -23,9 +23,9 @@
  * Requires Plugins:  woocommerce
  *
  * Requires at least:        6.7.0
- * Tested up to:             6.9.4
+ * Tested up to:             7.0
  * WC requires at least:     6.5.0
- * WC tested up to:          10.7.1
+ * WC tested up to:          10.8.1
  *
  * License:           GNU General Public License v3.0
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.html
