f43fab6422a102457677ced56c8abffe509fd30e
Shivamddeveloper <shivamsrivastava@wpswings.com>
2026-08-26 10:48:39 +0530

    language update

--- subscriptions-for-woocommerce.php
+++ subscriptions-for-woocommerce.php
@@ -23,9 +23,9 @@
  * Requires Plugins:  woocommerce
  *
  * Requires at least:        6.7
- * Tested up to:             7.0.2
+ * Tested up to:             7.1
  * WC requires at least:     6.5.0
- * WC tested up to:          10.9.4
+ * WC tested up to:          11.0.1
  *
  * License:           GNU General Public License v3.0
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.html
